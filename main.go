977448a03200ba7e6b0600ebbf437d94457c7e8f
jiuchengshaonian <291394199@qq.com>
2019-08-30 13:20:41 +0800

    在miango中添加路由

--- main.go
+++ main.go
@@ -14,7 +14,8 @@ func main()  {
 	http.HandleFunc("/file/download",handler.DownloadHandler)
 	http.HandleFunc("/file/update",handler.FileMetaUpdateHandler)
 	http.HandleFunc("/file/delete",handler.FileDeleteHandler)
-
+	http.HandleFunc("/file/query",handler.FileQueryHandler)
+ 
 	http.HandleFunc("/user/signup",handler.SignupHandler)
     http.HandleFunc("/user/signin",handler.SignInHandler)
     http.HandleFunc("/user/info",handler.HTTPInterceptor(handler.UserInfoHandler))
