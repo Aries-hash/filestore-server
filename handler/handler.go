360c877292733a5c1345c11b0cae4afd496bd1a4
jiuchengshaonian <291394199@qq.com>
2019-08-30 13:39:17 +0800

    在miango中添加路由

--- handler/handler.go
+++ handler/handler.go
@@ -115,6 +115,53 @@ func FileQueryHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write(data)
 }
 
+// TryFastUploadHandler : 尝试秒传接口
+func TryFastUploadHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+
+	// 1. 解析请求参数
+	username := r.Form.Get("username")
+	filehash := r.Form.Get("filehash")
+	filename := r.Form.Get("filename")
+	filesize, _ := strconv.Atoi(r.Form.Get("filesize"))
+
+	// 2. 从文件表中查询相同hash的文件记录
+	fileMeta, err := meta.GetFileMetaDB(filehash)
+	if err != nil {
+		fmt.Println(err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// 3. 查不到记录则返回秒传失败
+	if fileMeta == nil {
+		resp := util.RespMsg{
+			Code: -1,
+			Msg:  "秒传失败，请访问普通上传接口",
+		}
+		w.Write(resp.JSONBytes())
+		return
+	}
+
+	// 4. 上传过则将文件信息写入用户文件表， 返回成功
+	suc := dblayer.OnUserFileUploadFinished(
+		username, filehash, filename, int64(filesize))
+	if suc {
+		resp := util.RespMsg{
+			Code: 0,
+			Msg:  "秒传成功",
+		}
+		w.Write(resp.JSONBytes())
+		return
+	}
+	resp := util.RespMsg{
+		Code: -2,
+		Msg:  "秒传失败，请稍后重试",
+	}
+	w.Write(resp.JSONBytes())
+	return
+}
+
 // DownloadHandler : 文件下载
 func DownloadHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
--- main.go
+++ main.go
@@ -6,20 +6,23 @@ import(
 )
 
 func main()  {
+	//处理静态资源映射
 	http.Handle("/static/",http.StripPrefix("/static",http.FileServer(http.Dir("./static"))))
-	
-	http.HandleFunc("/file/upload",handler.UploadHandler)
-	http.HandleFunc("/file/upload/suc",handler.UploadSucHandler)
-	http.HandleFunc("/file/meta",handler.GetFileMetaHandler)
-	http.HandleFunc("/file/download",handler.DownloadHandler)
-	http.HandleFunc("/file/update",handler.FileMetaUpdateHandler)
-	http.HandleFunc("/file/delete",handler.FileDeleteHandler)
-	http.HandleFunc("/file/query",handler.FileQueryHandler)
- 
+	//url映射
+	//file
+	http.HandleFunc("/file/upload",handler.HTTPInterceptor(handler.UploadHandler))
+	http.HandleFunc("/file/upload/suc",handler.HTTPInterceptor(handler.UploadSucHandler))
+	http.HandleFunc("/file/meta",handler.HTTPInterceptor(handler.GetFileMetaHandler))
+	http.HandleFunc("/file/download",handler.HTTPInterceptor(handler.DownloadHandler))
+	http.HandleFunc("/file/update",handler.HTTPInterceptor(handler.FileMetaUpdateHandler))
+	http.HandleFunc("/file/delete",handler.HTTPInterceptor(handler.FileDeleteHandler))
+	http.HandleFunc("/file/query",handler.HTTPInterceptor(handler.FileQueryHandler))
+	http.HandleFunc("/file/fastupload",handler.HTTPInterceptor(handler.TryFastUploadHandler))
+    //user 
 	http.HandleFunc("/user/signup",handler.SignupHandler)
     http.HandleFunc("/user/signin",handler.SignInHandler)
     http.HandleFunc("/user/info",handler.HTTPInterceptor(handler.UserInfoHandler))
-    
+    //监听端口
 	err:=http.ListenAndServe(":8080",nil)
 	if err != nil {
 	   fmt.Printf("Failed to start server,err:%s",err.Error)
